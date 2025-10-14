af1ddc583ee575685e049ce9532ec8f8b5e64048
Stev6 <157082448+Stevv63@users.noreply.github.com>
2025-10-14 13:15:43 +0300

    License

--- src/main/java/stev6/nomindlessshooting/NoMindlessShooting.java
+++ src/main/java/stev6/nomindlessshooting/NoMindlessShooting.java
@@ -1,3 +1,10 @@
+/*
+    This file is part of NoMindlessShooting.
+    NoMindlessShooting is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+    NoMindlessShooting is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License along with NoMindlessShooting. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package stev6.nomindlessshooting;
 
 import com.mojang.logging.LogUtils;
